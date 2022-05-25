d3a89b862bd8632399eceb04f1cc031a725d2446
Jace <103338215+JSquaredJace@users.noreply.github.com>
2022-05-25 06:59:27 -0500

    Update Spacing on Comments, Headers
    Update spacing on comments, function headers, and file header including hardware configuration for readability.

--- With LCD Screen_Security System and Code Entry/main.c
+++ With LCD Screen_Security System and Code Entry/main.c
@@ -4,50 +4,50 @@
  * Created: 3/28/2020 11:26:17 AM
  *	  Rev 1	3/28/2020
  * Description:	ATMEGA2560 emulates a home security keypad with a screen. It
- *				does this by taking in information from a keypad matrix and
- *				outputting prompts and other information to the LCD screen.
- *				This program is based on the Security System with Code Entry
- *				program from lab 4 that uses a 4 digit 7 segment display
- *				instead of an LCD screen.
+ *		does this by taking in information from a keypad matrix and
+ *		outputting prompts and other information to the LCD screen.
+ *		This program is based on the Security System with Code Entry
+ *		program from lab 4 that uses a 4 digit 7 segment display
+ *		instead of an LCD screen.
  * 
  * Hardware:	ATMega 2560 operating at 16 MHz
- *				LCD screen
- *				10KOhm Potentiometer (POT)
- *				4x4 keypad module
- *				jumper wires
+ *		LCD screen
+ *		10KOhm Potentiometer (POT)
+ *		4x4 keypad module
+ *		jumper wires
  * Configuration shown below
  *
  * ATMega 2560
- *  PORT   pin		   LCD Screen
+ *  PORT   pin	       LCD Screen
  * -----------         ----------
- * |		 |	  GND--|K		|
- * |		 |	   5V--|A		|
- * | A7    29|---------|D7		|
- * | A6    28|---------|D6		|
- * | A5    27|---------|D5		|
- * | A4    26|---------|D4		|
- * |      	 |         |		|
- * | B1    52|---------|E		|
- * |		 |	  GND--|RW		|	10KOhm Potentiometer
- * | B0    53|---------|RS		|			POT
- * |      	 |         |		|		-----------
- * |		 |		   |	  V0|-------|W		5V|--5V
- * |	     |    5V---|VDD		|		|	   GND|--GND
- * |	  GND|---GND---|VSS		|		-----------
- * |		 |		   ----------
- * |		 |
- * |		 |			 Keypad
- * |		 | 		   ----------
- * | D3    18|---------|1		|
- * | D2    19|---------|2		|
- * | D1    20|---------|3		|
- * | D0    21|---------|4		|
- * |		 |		   |		|
- * | C0    37|---------|5		|
- * | C1    36|---------|6		|
- * | C2    35|---------|7		|
- * | C3    34|---------|8		|
- * -----------		   ----------
+ * |	     |	  GND--|K	|
+ * |	     |	   5V--|A	|
+ * | A7    29|---------|D7	|
+ * | A6    28|---------|D6	|
+ * | A5    27|---------|D5	|
+ * | A4    26|---------|D4	|
+ * |         |         |	|
+ * | B1    52|---------|E	|
+ * |	     |	  GND--|RW	|   10KOhm Potentiometer
+ * | B0    53|---------|RS	|	    POT
+ * |         |         |	|	-----------
+ * |	     |	       |      V0|-------|W	5V|--5V
+ * |	     |    5V---|VDD	|	|      GND|--GND
+ * |	  GND|---GND---|VSS	|	-----------
+ * |	     |	       ----------
+ * |	     |
+ * |	     |		 Keypad
+ * |	     | 	       ----------
+ * | D3    18|---------|1	|
+ * | D2    19|---------|2	|
+ * | D1    20|---------|3	|
+ * | D0    21|---------|4	|
+ * |	     |	       |	|
+ * | C0    37|---------|5	|
+ * | C1    36|---------|6	|
+ * | C2    35|---------|7	|
+ * | C3    34|---------|8	|
+ * -----------	       ----------
  *
  */ 
 
@@ -72,23 +72,23 @@ void displayMenu();
 void displayStart();
 
 
-int pin[4] = {-1, 0, 0, 0};			//stored pin number for arming system
+int pin[4] = {-1, 0, 0, 0};		//stored pin number for arming system
 int unlockPIN[4] = {-2, 0, 0, 0};	//unlock pin number for unlocking system
 
 int alarmEnable = 0;	//1 when alarm is enabled
-int PINset = 0;			//1 when pin has been set
+int PINset = 0;		//1 when pin has been set
 
 /*
  * Function:  main
  *  Calls functions to initialize ports, timers, and fill display array and to
- *	handle the commands from the key pad. Uses a forever loop to constantly 
- *	update the 4 digit 7 segment display and handle commands from the key pad
+ *  handle the commands from the key pad. Uses a forever loop to constantly 
+ *  update the 4 digit 7 segment display and handle commands from the key pad
  *
  *  returns:    0   successful program run.
  */
 int main(void)
 {	
-	LCD_init();			//initialize LCD screen
+	LCD_init();		//initialize LCD screen
 	initScrollStr();	//initialize scrolling text for LCD screen
 	initKeypad();		//initialize keypad module
 	displayStart();		//scroll starting message
@@ -103,10 +103,10 @@ int main(void)
 /*
  * Function:  blinkMsg
  *  Blinks the input string on the LCD screen for 3 seconds. Each second, the 
- *	LCD screen shows message for 0.5 seconds, then shows a blank screen for 
- *	0.5 seconds.
+ *  LCD screen shows message for 0.5 seconds, then shows a blank screen for 
+ *  0.5 seconds.
  *
- *	str		char[40]	input string to be displayed
+ *  str		char[40]	input string to be displayed
  *
  *  returns:    none
  */
@@ -151,7 +151,7 @@ void err(){
 /*
  * Function:  start
  *  blinks all segments in display to indicate no pin has been set. Calls
- *	enterCode function to set pin if "A" or "C" buttons are pressed.
+ *  enterCode function to set pin if "A" or "C" buttons are pressed.
  *
  *  returns:    none
  */
@@ -166,10 +166,10 @@ void start(){
 	
 	while(confirm != 0){		//loop until pin is confirmed by user
 		if(enterCode(1) == 0){	//get acceptable PIN
-			confirm = succPIN();//confirm PIN with user
-			PINset = 1;			//PIN has been set
+			confirm = succPIN();	//confirm PIN with user
+			PINset = 1;		//PIN has been set
 		}
-		else{					//if pin is unsuccsessful, display error
+		else{				//if pin is unsuccsessful, display error
 			err();
 		}
 	}
@@ -179,13 +179,13 @@ void start(){
 /*
  * Function:  enterCode
  *  sets pin or unlock pin based on input from keypad and value of input param
- *	changePIN.
+ *  changePIN.
  *
- *	changePIN	0 changes the pin to unlock the system (unlockPIN)
- *				1 changes the stored pin (pin)
+ *  changePIN	0 changes the pin to unlock the system (unlockPIN)
+ *		1 changes the stored pin (pin)
  *
  *  returns:    0 entered pin is acceptable
- *				1 entered pin is unacceptable
+ *		1 entered pin is unacceptable
  */
 int enterCode(int changePIN){
 	int newKey = 0;		//next key pressed on keypad
@@ -244,11 +244,11 @@ int enterCode(int changePIN){
 /*
  * Function:  succPIN
  *  Displays a message showing the entered pin and prompts the user to confirm
- *	their pin or chose to enter a new one. This function recursively calls 
- *	itself on invalid input from keypad
+ *  their pin or chose to enter a new one. This function recursively calls 
+ *  itself on invalid input from keypad
  *
  *  returns:    0	pin confirmed by user
- *				1	user would like to enter a different pin	
+ *		1	user would like to enter a different pin	
  */
 int succPIN(){
 	int LCDline;
@@ -275,7 +275,7 @@ int succPIN(){
 	LCDline = 1;
 	LCD_write_str(message, &LCDline);
 	
-	getNewKey();			//wait for user option
+	getNewKey();		//wait for user option
 	confirm = pressedKey;	//get user option
 	
 	//if invalid key, error and call recursively call function
@@ -302,15 +302,15 @@ void enAlarm(){
 		int line;
 		char msg[13] = "System Armed";	//message for LCD
 		
-		line = 1;						//clear LCD lines
+		line = 1;			//clear LCD lines
 		LCD_clear_line(&line);
 		line = 0;
 		LCD_clear_line(&line);
 		
-		LCD_write_str(msg, &line);		//send message to LCD top line for 1s
+		LCD_write_str(msg, &line);	//send message to LCD top line for 1s
 		_delay_ms(1000);
 	}
-	else{					//if PIN has not been set, enter one
+	else{				//if PIN has not been set, enter one
 		pressedKey = 0xA;	//skip system startup message
 		start();
 	}
@@ -320,21 +320,21 @@ void enAlarm(){
 /*
  * Function:  disAlarm
  *  disables alarm if entered unlockPIN is the same as stored pin. Displays 
- *	error and returns to previous displayed message if alarm is not enabled or
- *	if entered unlockPIN does not match the stored pin.
+ *  error and returns to previous displayed message if alarm is not enabled or
+ *  if entered unlockPIN does not match the stored pin.
  *
  *  returns:    none
  */
 void disAlarm(){
 	int incorrectPIN = 0;	//flag for if incorrect pin is entered
-	int er = 0;				//flag for input pin error
-	int LCDline;			//LCD line
-	char msg[13];			//LCD message
+	int er = 0;		//flag for input pin error
+	int LCDline;		//LCD line
+	char msg[13];		//LCD message
 	
 	if(alarmEnable == 0){	//if alarm is not enabled, display error message 
-							//and return
+				//and return
 		
-		LCDline = 1;						//clear LCD lines
+		LCDline = 1;				//clear LCD lines
 		LCD_clear_line(&LCDline);
 		LCDline = 0;
 		LCD_clear_line(&LCDline);
@@ -354,27 +354,27 @@ void disAlarm(){
 	
 	for(int i = 0; i < 4; i++){		//loop to check if pin matches unlockPIN
 		if(pin[i] != unlockPIN[i])	//check each pin digit
-			incorrectPIN = 1;		//incorrctPIN = 1 if incorrect pin is 
-									//entered
+			incorrectPIN = 1;	//incorrctPIN = 1 if incorrect pin is 
+						//entered
 	}
 	
 	if(incorrectPIN == 1){	//if incorrect pin is entered, display message and
-							//(re)enable the alarm
+				//(re)enable the alarm
 		strcpy(msg, "Wrong PIN");
-		blinkMsg(msg);		//display error message
+		blinkMsg(msg);	//display error message
 		
-		enAlarm();			//(re)enable the alarm
+		enAlarm();	//(re)enable the alarm
 		return;
 	}
 	
-	alarmEnable = 0;		//disable alarm
+	alarmEnable = 0;	//disable alarm
 	
-	LCDline = 1;					//clear LCD lines
+	LCDline = 1;		//clear LCD lines
 	LCD_clear_line(&LCDline);
 	LCDline = 0;
 	LCD_clear_line(&LCDline);
 	
-	strcpy(msg, "Success");			//success message
+	strcpy(msg, "Success");		//success message
 	LCD_write_str(msg, &LCDline);	//send message to LCD top line for 1s
 	_delay_ms(1000);
 	return;
@@ -383,13 +383,13 @@ void disAlarm(){
 /*
  * Function:  selection
  *  calls start, enAlarm, and disAlarm functions based on current state of 
- *	system and input from keypad
+ *  system and input from keypad
  *
  *  returns:    none
  */
 void selection(){
 	if(PINset == 0){	//if pin is not set, call start function to indicate
-						//no pin is set and get pin
+				//no pin is set and get pin
 		start();
 		pressedKey = -1;	//clear pressedKey
 		return;
@@ -410,7 +410,7 @@ void selection(){
 	}
 	
 	if(pressedKey == 0xD){	//if "D" is pressed, call disAlarm to disable the
-		disAlarm();			//alarm
+		disAlarm();	//alarm
 		pressedKey = -1;	//clear pressedKey
 		return;
 	}
@@ -421,7 +421,7 @@ void selection(){
 /*
  * Function:  displayMenu
  *  prints menu options to the LCD screen for user to interact with security
- *	system.
+ *  system.
  *
  *  returns:    none
  */
@@ -449,6 +449,6 @@ void displayMenu(){
  */
 void displayStart(){
 	char str[20] = "System Setup    ";	//startup message
-	startScrollStr(str);				//scroll startup message
+	startScrollStr(str);			//scroll startup message
 	return;
 }
